afc2f21230cb83093f2fa2ea6a991058e4e6051f
William Raymond <william.c.raymond@protonmail.com>
2022-08-07 17:30:19 -0400

    Add cart logic

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -14,12 +14,31 @@ export class CartService {
   }
 
   addProductToCart(product: Product): Product[] {
-    this.cartProducts.push(product);
+    if(this.productAlreadyAdded(product.id)) {
+      const productIndex = this.cartProducts.findIndex(p => p.id === product.id);
+      this.cartProducts[productIndex].quantity += product.quantity;
+    } else {
+      this.cartProducts.push(product);
+    }
+    return this.cartProducts;
+  }
+
+  updateQuantity(productId: number, newQuantity: number): Product[] {
+    const productIndex = this.cartProducts.findIndex(p => p.id === productId);
+    this.cartProducts[productIndex].quantity = newQuantity;
     return this.cartProducts;
   }
 
   clearCartProducts(): Product[] {
     this.cartProducts = [];
     return this.cartProducts;
+  }
+
+  private productAlreadyAdded(id: number): boolean {
+    const isAdded = this.cartProducts.some(product => {
+      if (product.id === id) return true;
+      else return false;
+    });
+    return isAdded;
   }
 }
