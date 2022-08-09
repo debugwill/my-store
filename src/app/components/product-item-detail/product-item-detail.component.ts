80a1b46e80d3adabc5dcce6c12503bb931809f01
William Raymond <william.c.raymond@protonmail.com>
2022-08-09 10:15:01 -0400

    Start work on product detail comp

---
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-item-detail',
+  templateUrl: './product-item-detail.component.html',
+  styleUrls: ['./product-item-detail.component.css']
+})
+export class ProductItemDetailComponent implements OnInit {
+  productId: number;
+  product: Product;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private productService: ProductService
+  ) {
+    this.product = new Product();
+    this.productId = 0;
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.productId = Number(params.get('id'));
+    });
+    this.productService.getProducts().subscribe(data => {
+      this.product = data.find(p => p.id === this.productId) as Product;
+    });
+    console.log(this.productId);
+    console.log(this.product);
+  }
+
+}
