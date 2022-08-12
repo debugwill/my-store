482f7422386900645de0fc7c0d5e1b20da527002
William Raymond <william.c.raymond@protonmail.com>
2022-08-12 09:18:26 -0400

    Fix basic unit tests

--- src/app/components/cart/cart.component.spec.ts
+++ src/app/components/cart/cart.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CartComponent } from './cart.component';
 
@@ -8,7 +9,8 @@ describe('CartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CartComponent ]
+      declarations: [ CartComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
 
