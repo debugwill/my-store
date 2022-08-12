480feddca895197aa95559aa9c48c348237cef2c
William Raymond <william.c.raymond@protonmail.com>
2022-08-12 09:18:26 -0400

    Fix basic unit tests

--- src/app/components/product-item-detail/product-item-detail.component.spec.ts
+++ src/app/components/product-item-detail/product-item-detail.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ProductItemDetailComponent } from './product-item-detail.component';
 
@@ -8,7 +10,11 @@ describe('ProductItemDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductItemDetailComponent ]
+      declarations: [ ProductItemDetailComponent ],
+      imports: [ 
+        RouterTestingModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
 
--- src/app/components/product-list/product-list.component.spec.ts
+++ src/app/components/product-list/product-list.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductListComponent } from './product-list.component';
@@ -8,7 +9,8 @@ describe('ProductListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductListComponent ]
+      declarations: [ ProductListComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
 
